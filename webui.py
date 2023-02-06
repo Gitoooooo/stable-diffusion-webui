41d49b9887cc7a23e7f0cd32cdaefc5144b29c5b
Gitoooooo <124330874+Gitoooooo@users.noreply.github.com>
2023-02-06 17:51:38 +0800

    0.0.17

--- webui.py
+++ webui.py
@@ -68,7 +68,7 @@ there are reports of issues with training tab on the latest version.
 Use --skip-version-check commandline argument to disable this check.
         """.strip())
 
-    expected_xformers_version = "0.0.16rc425"
+    expected_xformers_version = "0.0.17.dev441"
     if shared.xformers_available:
         import xformers
 
